e0195c81f90c2d1d7d5b63dd4cca23363f6970aa
Guilherme <guilherme.ioshimine@gmail.com>
2024-04-09 00:53:25 -0300

    Created Toolbar and SideNavBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidenavbarComponent } from './sidenavbar/sidenavbar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SidenavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [
     provideAnimationsAsync()
